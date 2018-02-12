9edf712b66d53b9a28cd1dbb25ee51e06ac2a244
Luca Francesca <luca@lucafrancesca.me>
2018-02-12 15:33:23 +0000

    Update main.py
    Added API version

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 def index():
     return "wikipedia keywords getter"
 
+@app.route("/api/version")
+def api_version():
+    return "0.1"
+
 @app.route('/api/getinfo/<string:id>/<int:n>/')
 def getinfo(id, n):
     return wiki_search.getData(id, n)
