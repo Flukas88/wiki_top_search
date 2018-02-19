3203781c8636e3bc44b3b3eb71111b5b37ec0a02
Luca Francesca <luca@lucafrancesca.me>
2018-02-19 18:06:37 +0000

    better api rest interface path

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 def index():
     return "wikipedia keywords getter"
 
-@app.route("/api/version")
-def api_version():
-    return "0.1"
+@app.route("/v1/version")
+def v1_version():
+    return "0.1.1"
 
-@app.route('/api/getinfo/<string:id>/<int:n>/')
+@app.route('/v1/getinfo/<string:id>/<int:n>/')
 def getinfo(id, n):
     return wiki_search.getData(id, n)
 
