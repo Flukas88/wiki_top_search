b08b02d3a6c81af3d9411fde42da14a0692f759c
Luca Francesca <luca@lucafrancesca.me>
2018-02-20 18:59:08 +0000

    Some pep8 formatting

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ import wiki_search
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return "wikipedia keywords getter"
+
 
 @app.route("/v1/version")
 def v1_version():
     return "0.1.1"
 
+
 @app.route('/v1/getinfo/<string:id>/<int:n>/')
 def getinfo(id, n):
     return wiki_search.getData(id, n)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- wiki_search.py
+++ wiki_search.py
@@ -8,25 +8,28 @@ import requests
 
 
 def _cleanWord(word):
-    """
-    (Internal function) Clean the input from those  chars (=, (, ), " , ', ",", \\n', .) 
-    Args: the word to be cleaned
-    Return: the cleaned word
-    """
-    return  word.replace('\n', '').replace('=', '').replace('(', '').replace(')', '').replace('"', '').replace(',', '').replace('.', '')
+
+
+"""Cleans word from chars that are not allowed
+
+Arguments:
+    word {string} -- the word to be cleaned
+"""
+    return word.replace('\n', '').replace('=', '').replace('(', '').replace(')', '').replace('"', '').replace(',', '').replace('.', '')
+
 
 def getData(id, n):
-    """
-    Get data \n
-    Args: 
-    - the *id* of the wikipedia page \n
-    - top *n* words to consider
 
-    Return: \n 
-    - end_string: the string containing the info requested or an invalid key message
-    """
+"""Get data from wikipedia
+
+Arguments:
+    id {string} -- the id of the page
+    n {int} -- descrithe top n elements
+"""
+
     end_string = ''
-    data = requests.get('https://en.wikipedia.org/w/api.php?action=query&prop=extracts&pageids={}&explaintext&format=json'.format(id))
+    data = requests.get(
+        'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&pageids={}&explaintext&format=json'.format(id))
     result = json.loads(data.text)
     try:
         info = result['query']['pages'][id]['extract']
@@ -36,7 +39,7 @@ def getData(id, n):
         return end_string
 
     tmp_words = [_cleanWord(el) for el in info.split(' ')]
-    words = filter(None, tmp_words) # remove empty strings
+    words = filter(None, tmp_words)  # remove empty strings
     words_dict = {}
 
     for word in words:
@@ -52,7 +55,8 @@ def getData(id, n):
         inv_map[v] = inv_map.get(v, [])
         inv_map[v].append(k)
 
-    end_string += "<b>URL:</b> <i>https://en.wikipedia.org/w/api.php?action=query&prop=extracts&pageids={}&explaintext&format=json</i></br> <b>Title</b>: {}</br>".format(id, title)
+    end_string += "<b>URL:</b> <i>https://en.wikipedia.org/w/api.php?action=query&prop=extracts&pageids={}&explaintext&format=json</i></br> <b>Title</b>: {}</br>".format(
+        id, title)
     end_string += "<b>Top <i>{}</i> words</b>:</br>".format(n)
     el_words = list(inv_map.values())
     el_count = list(inv_map.keys())
@@ -60,11 +64,10 @@ def getData(id, n):
     end_string += '<table style="width:70%; border: 2px solid black"><tr><th>Count</th><th>Words</th></tr>'
     for cnt in range(0, len(cnt_list)):
         words_string = ", ".join(el_words[cnt])
-
-        end_string += "<tr><td>{}</td><td>{}</td></tr>".format(cnt_list[cnt], words_string)
+        end_string += "<tr><td>{}</td><td>{}</td></tr>".format(
+            cnt_list[cnt], words_string)
     end_string += '</table>'
     return end_string
-
 
 
 if __name__ == "__main__":
