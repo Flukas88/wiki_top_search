9709cd5dd4950672ab52b911e09ef0f61995726c
Luca Francesca <luca@lucafrancesca.me>
2018-03-16 16:33:53 +0000

    Included ita language in Unittests

--- tests/Unittest.py
+++ tests/Unittest.py
@@ -10,9 +10,17 @@ class TopWordsTest(unittest.TestCase):
 
     def test_valid_id(self):
         self.assertIn("URL", get_data('21721040', 5))
-        
+
+
+    def test_valid_id_ita(self):
+        self.assertIn("URL", get_data('21724', 5, 'it'))
+
     def test_invalid_id(self):
         self.assertIn("invalid", get_data('2172104', 5))
+
+
+    def test_invalid_id_ita(self):
+        self.assertIn("invalid", get_data('21721040', 5, 'it'))
 
 if __name__ == '__main__':
     unittest.main()
