b5d1b338cd57360abd0bae3d22752b178ef1c892
Luca Francesca <luca@lucafrancesca.me>
2018-06-12 11:09:27 +0100

    Update wiki_search.py
    Import changes from my gitlab

--- src/wiki_search.py
+++ src/wiki_search.py
@@ -1,12 +1,18 @@
+"""
+ Get the top n keywords from wikipedia
+"""
 # -*- coding: utf-8 -*-
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+# pylint: disable=too-many-locals
+# pylint: disable=import-error
+# pylint: disable=bad-builtin
+
 from __future__ import division, absolute_import, print_function, unicode_literals
 import json
 import requests
 
 
 def clean_word(word):
-
     """Cleans word from chars that are not allowed
 
     Arguments:
@@ -16,13 +22,12 @@ def clean_word(word):
                .replace('"', '') .replace(',', '').replace('.', '')
 
 
-def get_data(page_id, n, lang='en'):
-
+def get_data(page_id, top_n, lang='en'):
     """Get data from Wikipedia
 
     Arguments:
         id {string} -- the id of the page
-        n {int} -- the top n elements
+        top_n {int} -- the top n elements
     """
 
     data = requests.get(
@@ -31,7 +36,7 @@ def get_data(page_id, n, lang='en'):
     result = json.loads(data.text)
     try:
         info = result['query']['pages'][page_id]['extract']
-        title = result['query']['pages'][page_id]['title']
+        #title = result['query']['pages'][page_id]['title']
     except (KeyError, TypeError):
         return None
 
@@ -48,14 +53,14 @@ def get_data(page_id, n, lang='en'):
             else:
                 words_dict[word] = 1
     inv_map = {}
-    
+
     for key, value in words_dict.items():
         inv_map[value] = inv_map.get(value, [])
         inv_map[value].append(key)
 
     el_words = list(inv_map.values())
     el_count = list(inv_map.keys())
-    cnt_list = sorted(el_count[0:n], reverse=True)
+    cnt_list = sorted(el_count[0:top_n], reverse=True)
     ret_word_dict = {}
     for cnt in range(0, len(cnt_list)):
         words_string = ", ".join(el_words[cnt])
